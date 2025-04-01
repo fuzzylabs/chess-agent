8a85c319795e82b0e4c295f30b674850d0848757
scott_clare <88718621+scott-clare1@users.noreply.github.com>
2025-04-01 14:25:36 +0100

    [FA-3] enable starting game and playing (#2)
    * feat: adding game start and play functionality
    
    * docs: updating documentation
    
    * docs: updating documentation
    
    * docs: fixing issue with markdwon image
    
    * docs: formatting readme
    
    * docs: removing trailing semi-colon
    
    * docs: making prompt block-quote
    
    * chore: removing trailing slash

--- agent_uno/server.py
+++ agent_uno/server.py
@@ -7,6 +7,9 @@ Aim:
 """
 import datetime
 
+import datetime
+from typing import cast
+
 from berserk import Client, TokenSession  # type: ignore [import-not-found]
 from mcp.server.fastmcp import FastMCP  # type: ignore [import-not-found]
 from pydantic import BaseModel, Field
@@ -26,6 +29,23 @@ class AccountInfo(BaseModel):
     followable: bool
     following: bool
     blocking: bool
+
+
+class CreatedGame(BaseModel):
+    """The response of creating a new game."""
+
+    id: str
+    rated: bool
+    variant: dict[str, str]
+    fen: str
+    turns: int
+    source: str
+    speed: str
+    perf: str
+    created_at: datetime.datetime = Field(alias="createdAt")
+    status: dict[str, str | int]
+    player: str
+    full_id: str = Field(alias="fullId")
 
 
 mcp = FastMCP("chess-mcp", dependencies=["berserk"])
@@ -48,3 +68,36 @@ async def login(api_key: str) -> None:
 async def get_account_info() -> AccountInfo:
     """Get the account info of the logged in user."""
     return AccountInfo(**session_state["client"].account.get())
+
+
+@mcp.tool(description="Create a new game.")  # type: ignore
+async def create_game() -> str:
+    """An endpoint for creating a new game."""
+    response = CreatedGame(
+        **session_state["client"].challenges.create_ai(color="black")
+    )
+
+    session_state["id"] = response.id
+
+    return f"You can view the game taking place here: https://lichess-org.github.io/api-demo/#!/game/{response.id}"
+
+
+@mcp.tool(description="End game.")  # type: ignore
+async def end_game() -> None:
+    """End the current game."""
+    session_state["client"].board.resign_game(session_state["id"])
+
+
+@mcp.tool(description="Get the current game state.")  # type: ignore
+async def get_game_state() -> str:
+    """Get the current game state."""
+    current_state = next(
+        session_state["client"].board.stream_game_state(session_state["id"])
+    )
+    return cast(str, current_state["state"]["moves"])
+
+
+@mcp.tool(description="Make a move.")  # type: ignore
+async def make_move(move: str) -> None:
+    """Make a move in the current game."""
+    session_state["client"].board.make_move(session_state["id"], move)
