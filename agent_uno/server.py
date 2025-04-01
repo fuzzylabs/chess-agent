f9f431a898ddffc07219781433b1b65d83ba8f2b
scott_clare <88718621+scott-clare1@users.noreply.github.com>
2025-04-01 14:28:24 +0100

    Fa 2 allowing login (#3)
    * feat: adding login func to mcp server
    
    * docs: updating README
    
    * docs: adding steps for creating lichess account and API key
    
    * chore: fixing link to chess UI
    
    * chore: fixing incorrect types
    
    * [FA-3] enable starting game and playing (#2)
    
    * feat: adding game start and play functionality
    
    * docs: updating documentation
    
    * docs: updating documentation
    
    * docs: fixing issue with markdwon image
    
    * docs: formatting readme
    
    * docs: removing trailing semi-colon
    
    * docs: making prompt block-quote
    
    * chore: removing trailing slash

--- agent_uno/server.py
+++ agent_uno/server.py
@@ -6,6 +6,7 @@ Aim:
 # 3. Agent is able to play the game by getting the current state and making moves.
 """
 import datetime
+from typing import cast
 
 from berserk import Client, TokenSession  # type: ignore [import-not-found]
 from mcp.server.fastmcp import FastMCP  # type: ignore [import-not-found]
@@ -28,6 +29,23 @@ class AccountInfo(BaseModel):
     blocking: bool
 
 
+class CreatedGame(BaseModel):
+    """The response of creating a new game."""
+
+    id: str
+    rated: bool
+    variant: dict[str, str]
+    fen: str
+    turns: int
+    source: str
+    speed: str
+    perf: str
+    created_at: datetime.datetime = Field(alias="createdAt")
+    status: dict[str, str | int]
+    player: str
+    full_id: str = Field(alias="fullId")
+
+
 mcp = FastMCP("chess-mcp", dependencies=["berserk"])
 
 session_state = {}
@@ -48,3 +66,36 @@ async def login(api_key: str) -> None:
 async def get_account_info() -> AccountInfo:
     """Get the account info of the logged in user."""
     return AccountInfo(**session_state["client"].account.get())
+
+
+@mcp.tool(description="Create a new game.")  # type: ignore
+async def create_game() -> str:
+    """An endpoint for creating a new game."""
+    response = CreatedGame(
+        **session_state["client"].challenges.create_ai(color="black")
+    )
+
+    session_state["id"] = response.id
+
+    return f"You can view the game taking place here: https://lichess-org.github.io/api-demo/#!/game/{response.id}"
+
+
+@mcp.tool(description="End game.")  # type: ignore
+async def end_game() -> None:
+    """End the current game."""
+    session_state["client"].board.resign_game(session_state["id"])
+
+
+@mcp.tool(description="Get the current game state.")  # type: ignore
+async def get_game_state() -> str:
+    """Get the current game state."""
+    current_state = next(
+        session_state["client"].board.stream_game_state(session_state["id"])
+    )
+    return cast(str, current_state["state"]["moves"])
+
+
+@mcp.tool(description="Make a move.")  # type: ignore
+async def make_move(move: str) -> None:
+    """Make a move in the current game."""
+    session_state["client"].board.make_move(session_state["id"], move)
